b4e9d28bb59ac8ac2ec6120204f3c79150d726a7
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-20 14:48:20 -0700

    add function to check if string is a palindrome
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- Palindromes/Models/PalindromesChecker.cs
+++ Palindromes/Models/PalindromesChecker.cs
@@ -4,5 +4,21 @@ namespace Palindromes.Models
   {
     public string UserInput {get; set;}
 
+    public string IsPalindrome()
+    {
+      int i = 0;
+      int j = UserInput.Length - 1;
+      while(i < j)
+      {
+        if(UserInput[i] != UserInput[j])
+        {
+          return "This is not a palindrome";
+        }
+        i++;
+        j--;
+      }
+      return "This is a palindrome";
+    }
+
   }
 }
